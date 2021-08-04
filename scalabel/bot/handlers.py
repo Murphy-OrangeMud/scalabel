f0405c19a17bdff4974b4bd12a5c27c97573fe2d
Murphy-OrangeMud <chengzhiyi_2@163.com>
2021-08-05 02:08:57 +0800

    Handlers

---
+++ scalabel/bot/handlers.py
@@ -0,0 +1,98 @@
+from io import BytesIO
+import os
+import time
+from PIL import Image
+from detectron2 import model_zoo
+from detectron2.checkpoint.detection_checkpoint import DetectionCheckpointer
+from detectron2.config.config import get_cfg
+from detectron2.modeling.meta_arch.build import build_model
+import detectron2.data.transforms as T
+import numpy as np
+from ts.torch_handler.vision_handler import VisionHandler
+import torch
+from torch.multiprocessing import Pool
+
+class Detectron2Handler:
+
+    def __init__(self):
+        self.model = None
+        self.device = None
+        self.initialized = False
+        self.context = None
+        self.manifest = None
+        self.map_location = None
+        self.aug = None
+
+    def initialize(self, context):
+        properties = context.system_properties
+        self.map_location = "cuda" if torch.cuda.is_available() and properties.get("gpu_id") is not None else "cpu"
+        self.device = torch.device(
+            self.map_location + ":" + str(properties.get("gpu_id"))
+            if torch.cuda.is_available() and properties.get("gpu_id") is not None
+            else self.map_location
+        )
+        self.manifest = context.manifest
+
+        model_dir = properties.get("model_dir")
+        serialized_file = self.manifest["model"]["serializedFile"]
+        model_pt_path = os.path.join(model_dir, serialized_file)
+
+        if not os.path.isfile(model_pt_path):
+            raise RuntimeError("Missing the model.pt file")
+
+        model_name = self.manifest["model"]["modelName"]
+        cfg_path = model_name.split(".")[0] + ".yaml"
+        cfg = get_cfg()
+        cfg.merge_from_file(model_zoo.get_config_file(cfg_path))
+        # NOTE: you may customize cfg settings
+        # cfg.MODEL.DEVICE="cuda" # use gpu by default
+        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+        # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
+        # you can also give a path to you checkpoint
+        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(cfg_path)
+
+        self.model = build_model(cfg)
+        self.model.eval()
+
+        checkpointer = DetectionCheckpointer(self.model)
+        checkpointer.load(cfg.MODEL.WEIGHTS)
+
+        self.aug = T.ResizeShortestEdge(
+            [cfg.INPUT.MIN_SIZE_TEST, cfg.INPUT.MIN_SIZE_TEST], cfg.INPUT.MAX_SIZE_TEST
+        )
+        
+        self.initialized = True
+
+    def inference(self, data, *args, **kwargs):
+        with torch.no_grad():
+            predictions = self.model(data)
+            return predictions
+
+    def handle(self, data, context):
+        start_time = time.time()
+        
+        self.context = context
+        metrics = self.context.metrics
+
+        results = self.inference(self.preprocess(data))
+
+        stop_time = time.time()
+        metrics.add_time("HandlerTime", round((stop_time - start_time) * 1000, 2), None, "ms")
+
+        return results
+
+    def preprocess(self, data):
+        # data: List of Bytes representing pictures
+        images = []
+        for item in data:
+            img = np.array(Image.open(BytesIO(item.content)))
+            height, width = img.shape[:2]
+            img = self.aug.get_transform(img).apply_image(img)
+            img = torch.as_tensor(img.astype("float32").transpose(2, 0, 1))
+            if self.map_location == "cuda":
+                img = img.pin_memory()
+                img = img.cuda(non_blocking=True)
+            images.append({"image": img, "height": height, "width": width})
+        
+        return images
+
--- scalabel/bot/server.py
+++ scalabel/bot/server.py
@@ -36,6 +36,9 @@ class ModelServerScheduler(object):
 
         self.logger = logger
 
+    def gen_model_id(self):
+        pass
+
     # restore when server restarts, connects to redis channels.
     def restore(self):
         pass
@@ -74,8 +77,10 @@ class ModelServerScheduler(object):
 
         # here needs to change 
         model_name = self.tasks[f'{project_name}_{task_id}']["model"]
+        image_dict = self.tasks[f'{project_name}_{task_id}']["image_dict"]
+        input_data = [image_dict[item["url"]] for item in items]
         # 似乎torchserve只支持部署在本地（服务器），即只支持单机，不支持分布式调度
-        results = requests.get("http://127.0.0.1:8080/predictions/%s" % model_name)
+        results = requests.post(url="http://127.0.0.1:8080/predictions/%s" % model_name, data=input_data)
 
         pred_boxes: List[List[float]] = []
         for box in results[0]["instances"].pred_boxes:
@@ -88,12 +93,13 @@ class ModelServerScheduler(object):
     # 假定每个register的task用的model不是同一个
     def register_task(self, project_name, task_id, item_list):
         self.logger.info("RegisterTask running...")
-        model_name = self.deploy_model(self.model_config["model_name"], item_list)
+        model_name, image_dict = self.deploy_model(self.model_config["model_name"], item_list)
 
         self.tasks[f'{project_name}_{task_id}'] = {
             "project_name": project_name,
             "task_id": task_id,
             "model": model_name,
+            "image_dict": image_dict
         }
 
         model_request_channel = self.model_request_channel % (project_name, task_id)
@@ -108,72 +114,39 @@ class ModelServerScheduler(object):
         image_dict = {}
 
         def load_inputs(item_list: List, num_workers: int) -> None:
-            def url_to_img(url: str, aug: object, device: str) -> Dict:
+            def url_to_img(url):
                 img_response = requests.get(url)
-                img = np.array(Image.open(BytesIO(img_response.content)))
-                height, width = img.shape[:2]
-                img = aug.get_transform(img).apply_image(img)
-                img = torch.as_tensor(img.astype("float32").transpose(2, 0, 1))
-                if device == "cuda":
-                    img = img.pin_memory()
-                    img = img.cuda(non_blocking=True)
-                return {"image": img, "height": height, "width": width}
+                return img_response
 
             urls = [item["urls"]["-1"] for item in item_list]
             if num_workers > 1:
                 pool = Pool(num_workers)
-                image_list = list(pool.starmap(url_to_img,
-                                            zip(urls,
-                                                [aug] * len(urls),
-                                                [cfg.MODEL.DEVICE] * len(urls)
-                                                )))
+                image_list = list(pool.starmap(url_to_img, urls))
             else:
-                image_list = [url_to_img(url, aug, cfg.MODEL.DEVICE) for url in urls]
+                image_list = [url_to_img(url) for url in urls]
 
             for url, image in zip(urls, image_list):
                 image_dict[url] = image
 
-        cfg_path = model_name
-        cfg = get_cfg()
-        cfg.merge_from_file(model_zoo.get_config_file(cfg_path))
-        # NOTE: you may customize cfg settings
-        # cfg.MODEL.DEVICE="cuda" # use gpu by default
-        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
-        # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
-        # you can also give a path to you checkpoint
-        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(cfg_path)
-
-        model = build_model(cfg)
-        model.eval()
-
-        checkpointer = DetectionCheckpointer(model)
-        checkpointer.load(cfg.MODEL.WEIGHTS)
-
-        aug = T.ResizeShortestEdge(
-            [cfg.INPUT.MIN_SIZE_TEST, cfg.INPUT.MIN_SIZE_TEST], cfg.INPUT.MAX_SIZE_TEST
-        )
-
         load_inputs(item_list, NUM_WORKERS)
         
-        model_name = hash(model)
-        serialized_file = ""  # ?
+        model_id = model_name + self.gen_model_id()
         export_path = "model_store"
-        handler = "" # ?
+        handler = "./handlers.py"
         
         torch_archive_cmd = "torch-model-archiver --model-name %s\
-                                --serialized-file %s \
                                 --export-path %s \
                                 --handler %s" \
-                                % (model_name, serialized_file, export_path, handler)
+                                % (model_name, export_path, handler)
         ts_start_cmd = "torchserve --start --ncs --model-store %s --models %s.mar" \
-                        % (export_path, model_name)
+                        % (export_path, model_id)
 
         os.system(torch_archive_cmd)
         self.logger.info("Model archived.")
         os.system(ts_start_cmd)
         self.logger.info("Torchserve started.")
 
-        return model_name
+        return model_id, image_dict
 
     def close(self):
         for thread_name, thread in self.threads.items():
