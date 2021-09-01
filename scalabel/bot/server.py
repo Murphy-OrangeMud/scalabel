3196c0d8fafa1235bb6a6fd2a8d147a9d2b73b6e
Murphy-OrangeMud <chengzhiyi_2@163.com>
2021-09-01 21:28:14 +0800

    run demo

--- scalabel/bot/server.py
+++ scalabel/bot/server.py
@@ -16,9 +16,21 @@ from torch.multiprocessing import Pool
 from io import BytesIO
 from PIL import Image
 import os
+import requests
 
 ray.init(address="auto", namespace="hello")
 serve.start(detached=True)
+
+def url_to_img(url, aug, device):
+    img_response = requests.get(url)
+    img = np.array(Image.open(BytesIO(img_response.content)))
+    height, width = img.shape[:2]
+    img = aug.get_transform(img).apply_image(img)
+    img = torch.as_tensor(img.astype("float32").transpose(2, 0, 1))
+    if device == "cuda":
+        img = img.pin_memory()
+        img = img.cuda(non_blocking=True)
+    return {"image": img, "height": height, "width": width}
 
 # Actor
 @ray.remote
@@ -29,6 +41,8 @@ class Predictor:
         cfg.merge_from_file(model_zoo.get_config_file(cfg_path))
         # NOTE: you may customize cfg settings
         # cfg.MODEL.DEVICE="cuda" # use gpu by default
+        os.environ["CUDA_VISIBLE_DEVICES"] = gpu_id
+        cfg.MODEL_DEVICE="cuda" if torch.cuda.is_available() else "cpu"
         cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
         # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
         # you can also give a path to you checkpoint
@@ -47,32 +61,19 @@ class Predictor:
 
         self.image_dict = {}
 
-        os.environ["CUDA_VISIBLE_DEVICE"] = gpu_id
         self.load_inputs(item_list, num_workers)
-
-    @staticmethod
-    def url_to_img(url, aug, device):
-        img_response = requests.get(url)
-        img = np.array(Image.open(BytesIO(img_response.content)))
-        height, width = img.shape[:2]
-        img = aug.get_transform(img).apply_image(img)
-        img = torch.as_tensor(img.astype("float32").transpose(2, 0, 1))
-        if device == "cuda":
-            img = img.pin_memory()
-            img = img.cuda(non_blocking=True)
-        return {"image": img, "height": height, "width": width}
 
     def load_inputs(self, item_list, num_workers):
         urls = [item["urls"]["-1"] for item in item_list]
         if num_workers > 1:
             pool = Pool(num_workers)
-            image_list = list(pool.starmap(self.url_to_img,
+            image_list = list(pool.starmap(url_to_img,
                                            zip(urls,
                                                [self.aug] * len(urls),
                                                [self.cfg.MODEL.DEVICE] * len(urls)
                                                )))
         else:
-            image_list = [self.url_to_img(url, self.aug, self.cfg.MODEL.DEVICE) for url in urls]
+            image_list = [url_to_img(url, self.aug, self.cfg.MODEL.DEVICE) for url in urls]
 
         for url, image in zip(urls, image_list):
             self.image_dict[url] = image
@@ -81,15 +82,20 @@ class Predictor:
         inputs = [self.image_dict[item["url"]] for item in items]
         with torch.no_grad():
             predictions = self.model(inputs)
-            return predictions
 
-@serve.deployment
+            pred_boxes: List[List[float]] = []
+            for box in predictions[0]["instances"].pred_boxes:
+                box = box.cpu().numpy()
+                pred_boxes.append(box.tolist())
+            return pred_boxes
+
+@serve.deployment(num_replicas=1)
 class ModelServerScheduler(object):
     def __init__(self, server_config, model_config, logger):
         self.server_config = server_config
         self.model_config = model_config
 
-        self.redis = redis.Redis(host=server_config["redis_host"], port=server_config["redis_port"])
+        self.redis = redis.Redis(host=server_config["redis_host"], port=server_config["redis_port"], health_check_interval=3)
 
         self.model_register_channel = "modelRegister"
         self.model_request_channel = "modelRequest_%s_%s"
@@ -100,7 +106,10 @@ class ModelServerScheduler(object):
         self.threads = {}
 
         self.logger = logger
+<<<<<<< HEAD
         
+=======
+>>>>>>> run demo
         self.listen()
 
     def restore(self):
@@ -110,7 +119,6 @@ class ModelServerScheduler(object):
         pass
 
     def listen(self):
-        self.logger.info("Listening...")
         model_register_subscriber = self.redis.pubsub(ignore_subscribe_messages=True)
         model_register_subscriber.subscribe(**{self.model_register_channel: self.register_handler})
         thread = model_register_subscriber.run_in_thread(sleep_time=0.001)
@@ -118,7 +126,6 @@ class ModelServerScheduler(object):
         self.threads[self.model_register_channel] = thread
 
     def register_handler(self, register_message):
-        self.logger.info("RegisterHandler running...")
         register_message = json.loads(register_message["data"])
         project_name = register_message["projectName"]
         task_id = register_message["taskId"]
@@ -127,8 +134,10 @@ class ModelServerScheduler(object):
         self.register_task(project_name, task_id, item_list)
 
         self.logger.info(f"Set up model inference for {project_name}: {task_id}.")
+        print(f"Set up model inference for {project_name}: {task_id}.")
 
     def request_handler(self, request_message):
+        print("REquest handler")
         request_message = json.loads(request_message["data"])
         project_name = request_message["projectName"]
         task_id = request_message["taskId"]
@@ -139,15 +148,10 @@ class ModelServerScheduler(object):
         model = self.tasks[f'{project_name}_{task_id}']["model"]
 
         results = model.predict.remote(items)
-        results = ray.get(results)
-
-        pred_boxes: List[List[float]] = []
-        for box in results[0]["instances"].pred_boxes:
-            box = box.cpu().numpy()
-            pred_boxes.append(box.tolist())
+        results = ray.get(results, timeout=300)
 
         model_response_channel = self.model_response_channel % (project_name, task_id)
-        self.redis.publish(model_response_channel, json.dumps([pred_boxes, item_indices, action_packet_id]))
+        self.redis.publish(model_response_channel, json.dumps([results, item_indices, action_packet_id]))
 
     def register_task(self, project_name, task_id, item_list):
         model = self.get_model(self.model_config["model_name"], item_list)
@@ -167,8 +171,8 @@ class ModelServerScheduler(object):
 
     # 在171行，ray会直接把这个actor对象放到一个有gpu的node上并调用构造函数。不需要手动放。需要做的就是在构造函数里指定CUDA_VISIBLE_ENVIRONMENT
     def get_model(self, model_name, item_list):
-        NUM_WORKERS = 8
-        model = Predictor.remote(model_name, item_list, NUM_WORKERS, self.logger, "0")
+        NUM_WORKERS = 1
+        model = Predictor.remote(model_name, item_list, NUM_WORKERS, self.logger, "1")
         return model
 
     def put_model(self, model):
@@ -194,7 +198,6 @@ def launch():
     model_config = {
         "model_name": "COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"
     }
-
     ModelServerScheduler.deploy(server_config, model_config, logger)
     logger.info("Model server launched.")
 
